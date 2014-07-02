6bcd037544f76e29fde2d2a24b5810eb5906e83a
Marvin Arnold <marvinmarnold@gmail.com>
2014-07-02 13:48:28 +0200

    Copy skeleton from original heekari repo

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def products
+  end
+
+  def team
+  end
+
+  def save
+  end
+
+  def shop
+  end
+
+  def connect
+  end
+end
